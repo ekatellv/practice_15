e547eb571458f64e47aaa14717eed9bcf2d4b23a
ekatellv <159680448+ekatellv@users.noreply.github.com>
2025-12-08 21:02:57 +0700

    Update ex_5.py

--- ex_5.py
+++ ex_5.py
@@ -3,15 +3,14 @@ def mod_number(a: int, b: int) -> int:
     """
     Recursively calculates the remainder of dividing natural number a by natural number b.
     
-    Parameters
+    Arguments:
     a : int
         The dividend (natural number to be divided)
     b : int
         The divisor (natural number to divide by)
         
-    Returns
-    int
-        The remainder of a divided by b
+    Returns:
+    int: the remainder of a divided by b
     """
   
     if b == 0:
