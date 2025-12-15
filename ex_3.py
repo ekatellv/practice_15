f424d68643cc3fbcd4edf67338b84316924d1d3f
ekatellv <159680448+ekatellv@users.noreply.github.com>
2025-12-15 18:21:31 +0700

    Update ex_3.py

--- ex_3.py
+++ ex_3.py
@@ -12,5 +12,4 @@ def progress(a1: float, r: float, n: int) -> float:
     """
     if n == 1:
         return a1
-    else:
-        return r + progress(a1, r, n - 1)
+    return r + progress(a1, r, n - 1)
