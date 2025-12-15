e588ce98369ebaf7a5e808deb455b7fb1c3cab39
ekatellv <159680448+ekatellv@users.noreply.github.com>
2025-12-15 18:58:13 +0700

    Update ex_4.py

--- ex_4.py
+++ ex_4.py
@@ -12,5 +12,4 @@ def sum_progress(a1: float, r: float, n: int) -> float:
     """
     if n == 1:
         return a1
-    else:
-        return (a1 + (n - 1) * r) + sum_progress(a1, r, n - 1)
+    return (a1 + (n - 1) * r) + sum_progress(a1, r, n - 1)
