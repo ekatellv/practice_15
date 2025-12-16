c1bce3dfeb0f2fe74e00f98a843ddfcb8e2b3310
ekatellv <159680448+ekatellv@users.noreply.github.com>
2025-12-16 08:29:51 +0700

    Create ex_12.py

---
+++ ex_12.py
@@ -0,0 +1,21 @@
+def search(a: list, x: int) -> int:
+    """
+    Search for an integer x in a list a using recursion.
+
+    Parameters:
+    a : list
+        List of integers to search in
+    x : int
+        Integer to search for
+
+    Returns:
+    int
+        1 if x is found in the list, 0 otherwise
+    """
+
+    if not a:
+        return 0
+    if a[0] == x:
+        return 1
+
+    return search(a[1:], x)
